08fe57ea7983901ec2dab6732d210d98934d1cfe
Joshua Green <joshuag331@gmail.com>
2019-06-27 01:30:39 +0000

    updates to twenty_one program and testing environment for twenty_one

--- lesson_6/test_twenty_one.rb
+++ lesson_6/test_twenty_one.rb
@@ -1,31 +1,5 @@
 require 'pry'
 require 'pry-byebug'
-=begin
-1. Initialize deck
-  deck = { "H" => ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-           "D" => ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-           "S" => ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-           "C" => ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-  }
-  
-  Initialize Hands: Player and Dealer
-
-
-2. Deal cards to player and dealer
-  Given a deck,
-    select two cards from deck and set them equal to a variable, delete the cards from the deck
-    Set Player hands = the two cards that were deleted from the deck
-    select two more cards from the deck and set them equal to a variable, delete the cards from the deck
-    
-
-3. Player turn: hit or stay
-  - repeat until bust or "stay"
-4. If player bust, dealer wins.
-5. Dealer turn: hit or stay
-  - repeat until total >= 17
-6. If dealer bust, player wins.
-7. Compare cards and declare winner.
-=end
 
 PLAYER_WINNING_SCORE = 21
 DEALER_WINNING_SCORE = 17
@@ -39,7 +13,6 @@ def clear
 end
 
 deck = {}
-
 hands = { player: [], dealer: [] }
 
 def initialize_deck(dck)
@@ -75,27 +48,26 @@ end
 def play_again?
   answer = ''
   puts "------------"
-  
+
   loop do
     prompt "Do you want to play again? (y/n)"
     answer = gets.chomp.downcase
     break if valid_answer?(answer, "y", "n")
     prompt "That is not a valid response."
   end
-  
+
   answer
 end
 
 def hit_or_stay? # asks user to hit or stay and returns answer
   choice = ''
-  
   loop do
     prompt "Hit or Stay?"
     choice = gets.chomp.downcase
     break if valid_answer?(choice, "hit", "stay")
     prompt "That is not a valid choice!"
   end
-  
+
   choice
 end
 
@@ -106,7 +78,7 @@ end
 
 def calculate_total(values)
   sum = 0
-  
+
   values.each do |val|
     if ['J', 'Q', 'K'].include?(val)
       sum += 10
@@ -117,7 +89,7 @@ def calculate_total(values)
       sum += val.to_i
     end
   end
-  
+
   sum
 end
 
@@ -130,7 +102,9 @@ def calc_ace(total)
 end
 
 def bust(player_score)
-  player_score > PLAYER_WINNING_SCORE 
+
+  player_score > PLAYER_WINNING_SCORE
+
 end
 
 def dealer_bust(dealer_score)
@@ -146,7 +120,11 @@ def determine_winner(p_score, d_score, p_choice, d_choice)
     return "Dealer"
   elsif dealer_bust(d_score)
     return "Player"
-  elsif p_choice == 'stay' && d_choice == 'stay'
+  elsif (p_score == PLAYER_WINNING_SCORE) && (d_score != PLAYER_WINNING_SCORE)
+    return "Player"
+  elsif (d_score == PLAYER_WINNING_SCORE) && (p_score != PLAYER_WINNING_SCORE)
+    return "Dealer"
+  elsif (p_choice == 'stay') && (d_choice == 'stay')
     return calculate_final_score(p_score, d_score)
   else
     "Tie"
@@ -155,11 +133,11 @@ end
 
 def join_cards(arr)
   temp_arr = arr
-  if arr.size == 2 
-    arr.join(" and ")
+  if temp_arr.size == 2
+    temp_arr.join(" and ")
   else
     temp_arr[-1] = "and #{arr.last}"
-    arr.join(", ")
+    temp_arr.join(", ")
   end
 end
 
@@ -179,7 +157,6 @@ end
 
 loop do
   clear
-  
   score = { player: 0, dealer: 0 }
   choice = ''
   dealer_choice = ''
@@ -188,33 +165,34 @@ loop do
   hands = { player: [], dealer: [] }
 
   loop do
-    initialize_deck(deck) 
+    initialize_deck(deck)
     initialize_hand(hands, :player, deck) # create player hand
     initialize_hand(hands, :dealer, deck) # create dealer hand
     prompt "Welcome to 21!"
-    binding.pry
+
     loop do
       score[:dealer] = calculate_total(hands[:dealer])
       score[:player] = calculate_total(hands[:player])
-      prompt "Dealer has: #{hnds[dealr][0]} and unknown card."
-      prompt "You have: #{join_cards(hnds[plyer])} for a total of #{scre[plyer]}."
-      busted?(score[:player], score[:dealer])
-      break if bust(score[:player])
+      prompt "Dealer has: #{hands[:dealer][0]} and unknown card."
+      prompt "You have: #{join_cards(hands[:player])} for a total of #{score[:player]}."
       #binding.pry
       choice = hit_or_stay?
       #binding.pry
       break if choice == "stay"
       hit(deck, hands, :player)
       prompt "You hit!"
+      break if bust(score[:player])
       #binding.pry
     end
-    
+
+    busted?(score[:player], score[:dealer])
     winner = determine_winner(score[:player], score[:dealer], choice, dealer_choice)
-    prompt "Dealer Turn..." unless bust(score[:player])
+    prompt "Dealer Turn..." unless bust(score[:player]) || winner != nil
     sleep 2
-    dealer_choice = "stay"
-    
+
     loop do
+      break if winner
+      break if bust(score[:player]) || dealer_bust(score[:dealer])
       score[:dealer] = calculate_total(hands[:dealer])
       display_cards(hands, score, :player, :dealer)
       break if dealer_bust(score[:dealer]) || bust(score[:player]) || winner
@@ -223,16 +201,18 @@ loop do
       #binding.pry
       sleep 7
     end
+
     #binding.pry
+    sleep 3
+
     display_cards(hands, score, :player, :dealer)
     winner = determine_winner(score[:player], score[:dealer], choice, dealer_choice) if !winner
-    binding.pry
-    busted?(score[:player], score[:dealer])
-    winner = "Dealer" if bust(score[:player]) 
+    winner = "Dealer" if bust(score[:player])
     prompt "The winner is #{winner}!" unless winner == "Tie"
     tie(winner)
     break if winner
   end
+
   answer = play_again?
   break if answer == "n"
 end
